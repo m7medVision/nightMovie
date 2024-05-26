d98151e693b2cfbcef2a23c26279226bcf4f8b87
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-05-26 16:42:59 +0400

    add alert comp. && working in login page

--- src/app/(app)/login/page.tsx
+++ src/app/(app)/login/page.tsx
@@ -1,6 +1,48 @@
-export default function LoginPage() {
+"use client"
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { useForm } from 'react-hook-form'
+export default function LoginPage({ searchParams }: { searchParams: { message: string } }) {
+  const form = useForm()
   return (
-    <div>LoginPage</div>
+    <form className="h-screen flex items-center">
+      <Card className="w-full max-w-sm mx-auto my-auto">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>Enter your email below to login to your account.</CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          {searchParams.message && (
+            <Alert variant={'destructive'}>
+              <AlertTitle>Error</AlertTitle>
+              <AlertDescription>{searchParams.message}</AlertDescription>
+            </Alert>
+          )}
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" name='email' type="email" placeholder="m@example.com" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" name="password" type="password" required placeholder="**********" />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button type="submit" className="w-full">
+            Sign in
+          </Button>
+        </CardFooter>
+      </Card>
+    </form>
   )
 }
-
