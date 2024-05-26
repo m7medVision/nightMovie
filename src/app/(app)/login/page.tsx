377af4c4f8cececbf142937d85331ef88a08339e
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-05-26 20:49:44 +0400

    add login

--- src/app/(app)/login/page.tsx
+++ src/app/(app)/login/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import { Button } from '@/components/ui/button'
 import {
   Card,
@@ -9,40 +9,74 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { useForm } from 'react-hook-form'
-export default function LoginPage({ searchParams }: { searchParams: { message: string } }) {
-  const form = useForm()
+import { Form, FormField, FormLabel } from '@/components/ui/form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+export default function LoginPage() {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+  })
+  async function onSubmit(data: z.infer<typeof loginSchema>) {
+    const { email, password } = data
+    const res = await fetch('/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+    const json = await res.json()
+    if (!res.ok) {
+      form.setError("root", { message: json.error })
+    } else {
+      window.location.href = '/find'
+    }
+  }
   return (
-    <form className="h-screen flex items-center">
-      <Card className="w-full max-w-sm mx-auto my-auto">
-        <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>Enter your email below to login to your account.</CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          {searchParams.message && (
-            <Alert variant={'destructive'}>
-              <AlertTitle>Error</AlertTitle>
-              <AlertDescription>{searchParams.message}</AlertDescription>
-            </Alert>
-          )}
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" name='email' type="email" placeholder="m@example.com" required />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" name="password" type="password" required placeholder="**********" />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button type="submit" className="w-full">
-            Sign in
-          </Button>
-        </CardFooter>
-      </Card>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="h-screen flex items-center">
+        <Card className="w-full max-w-sm mx-auto my-auto">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>Enter your email below to login to your account.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <div className="grid gap-2">
+                  <FormLabel>Email</FormLabel>
+                  <Input {...field} type="email" placeholder="" required />
+                </div>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <div className="grid gap-2">
+                  <FormLabel>Password</FormLabel>
+                  <Input {...field} type="password" placeholder="" required />
+                </div>
+              )}
+            />
+          </CardContent>
+          <CardFooter>
+            <Button type="submit" className="w-full">
+              Sign in
+            </Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </Form>
   )
 }
